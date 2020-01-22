18380b37f926b219c42924d902f71be9ec617def
Matt Harp <matthewharp@gmail.com>
2020-01-21 18:03:19 -0600

    wip

--- src/actions/signUp/SignUpActions.js
+++ src/actions/signUp/SignUpActions.js
@@ -41,13 +41,7 @@ export function createUser(userCreationForm, forcedTime = undefined) {
         })
             .then(response => response.json())
             .then(json => {
-                if (json.err) {
-                    dispatch(createUserFailure(json.err[0], forcedTime));
-                } else if (json.user) {
-                    dispatch(createUserSuccess(json.user, forcedTime));
-                } else {
-                    dispatch(createUserFailure("Unknown API response.", forcedTime));
-                }
+                dispatch(createUserFailure(json, 1));
             });
     }
 }
